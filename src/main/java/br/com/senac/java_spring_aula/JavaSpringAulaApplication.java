8de07a09ac3a8d20bf1eabd38f32b8d5180e5acf
Jean <jjean3364@gmail.com>
2026-07-07 20:02:05 -0300

    Adicionado projeto spring

---
+++ src/main/java/br/com/senac/java_spring_aula/JavaSpringAulaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.java_spring_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/java_spring_aula/JavaSpringAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.java_spring_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
