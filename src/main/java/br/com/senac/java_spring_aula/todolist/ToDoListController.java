795a584cc1dc95a7fd7b17284a33054547ad759c
Jean <jjean3364@gmail.com>
2026-07-09 18:59:37 -0300

    conteudo ultima aula

---
+++ src/main/java/br/com/senac/java_spring_aula/todolist/ToDoListController.java
@@ -0,0 +1,10 @@
+package br.com.senac.java_spring_aula.todolist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lista")
+public class ToDoListController {
+
+}
---
+++ src/main/java/br/com/senac/java_spring_aula/todolist/model/ListaEntity.java
@@ -0,0 +1,29 @@
+package br.com.senac.java_spring_aula.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "lista")
+@Getter
+@Setter
+public class ListaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
+
+    private String status;
+
+}
